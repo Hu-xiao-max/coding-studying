4e92f8ad5fe8d95dddb7672f03a94055c3923de0
Hu-xiao-max <huxiao2116758@163.com>
2023-07-27 17:11:09 +0800

    tmp

---
+++ algorithm/search/binary_search.py
@@ -0,0 +1,16 @@
+#二分查找查找的是索引，所以在使用递归调用的时候需要传入索引的参数来控制递归的传入传出
+
+def bs(arr,start,end,target):
+    mid=int(start+(end-start)/2)
+    if arr[mid]==target:
+        return mid
+    elif arr[mid]<target:
+        return bs(arr,mid,end,target)
+    else:
+        return bs(arr,start,mid,target)
+arr = [ 2, 3, 4, 10, 40 ] 
+target = 10
+print(bs(arr,0,len(arr)-1,target))
+
+
+
